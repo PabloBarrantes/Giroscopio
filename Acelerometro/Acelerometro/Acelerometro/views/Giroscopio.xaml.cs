22a808390b01ef6af7fbca8b43f3bcde65e05bbf
PabloBarrantes <pabh92@gmail.com>
2018-06-06 10:00:40 +0200

    First commit

---
+++ Acelerometro/Acelerometro/Acelerometro/views/Giroscopio.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Acelerometro.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Giroscopio : ContentPage
+    {
+        SensorSpeed speed = SensorSpeed.Normal;
+
+        public Giroscopio()
+        {
+            InitializeComponent();
+            Gyroscope.ReadingChanged += Gyroscope_ReadingChanged;
+            this.IniciarGiroscopio.Clicked += IniciarGiroscopio_Clicked;
+            this.PararGiroscopio.Clicked += PararGiroscopio_Clicked;
+        }
+
+        private void PararGiroscopio_Clicked(object sender, EventArgs e)
+        {
+            Gyroscope.Stop();
+        }
+
+        private void IniciarGiroscopio_Clicked(object sender, EventArgs e)
+        {
+            Gyroscope.Start(speed);
+        }
+
+        private void Gyroscope_ReadingChanged(GyroscopeChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process Angular Velocity X, Y, and Z
+            this.resultado.Text = "Reading: X:" + data.AngularVelocity.X + ", Y:" + data.AngularVelocity.X + ", Z:" + data.AngularVelocity.Z;
+
+        }
+
+    }
+}
